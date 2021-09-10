c1c3623542acb2789a8fea6b257dcaaa2421b4a8
thibaut <thibaut.abellaneda@isen.yncrea.fr>
2021-09-10 15:47:59 +0200

    admin ui implementation

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -4,7 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import NavBar from './NavBar'
 import RepositoryTable from './RepositoryTable'
 import TagsTable from './TagsTable'
-import ManifestCard from './ManifestCard'
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 import 'typeface-roboto';
@@ -14,19 +13,14 @@ class MainView extends Component {
     constructor(props) {
         super(props);
         this.tagTable = React.createRef();
-        this.manifestCard = React.createRef();
-    }
-
-    clickRepository = (event, rowData) => {
-        this.manifestCard.current.setManifest(rowData)
-    }
-
-    clickTag = (event, rowData) => {
-        this.manifestCard.current.setManifest(rowData)
-    }
+	}
 
     lendBook = (event) => {
         this.tagTable.current.lendBook(event)
+    }
+	
+	returnBook = (event) => {
+        this.tagTable.current.returnBook(event)
     }
 
     render() {
@@ -37,9 +31,7 @@ class MainView extends Component {
                 <Grid item xs={4}>
                     <RepositoryTable onClick={this.clickRepository} />
                 </Grid>
-                <Grid item xs={4}>
-                     <ManifestCard lendBookTrigger={this.lendBook} innerRef={this.manifestCard} />
-                </Grid>
+
                 <Grid item xs={4}>
                     <TagsTable ref={this.tagTable} onClick={this.clickTag} />
                 </Grid>
--- src/components/RepositoryTable.js
+++ src/components/RepositoryTable.js
@@ -1,90 +1,85 @@
 import React, { Component } from 'react';
 import MaterialTable from 'material-table'
 import {CastByteToNumber} from '../helpers.js'
-import { Button, Table, TableRow, TableCell, TableContainer, TableHead, TablePagination, Toolbar, Typography, Paper, Checkbox, IconButton, Tooltip, FormControlLabel, Switch } from'@material-ui/core'
-import {DeleteIcon, FilterList, Cancel, CheckCircleIcon} from '@material-ui/icons';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CancelIcon from '@material-ui/icons/Cancel';
+import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableRow from '@material-ui/core/TableRow';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { lighten, makeStyles } from '@material-ui/core/styles';
 
-const columns=[
+function RepositoryTable() {
+  const { useState } = React;
+
+  const [columns, setColumns] = useState([
     { title: 'Name', field: 'bookname' },
     { title: 'Autor', field: 'bookauteur'},
     { title: 'Available', field: 'bookavailable', render: rowData => {
         return (rowData.bookavailable) ? <CheckCircleIcon /> : <CancelIcon />
         } 
     }
-]
+  ]);
 
-
-const options = {
-    pageSize: 8
-};
-
-const items = [
+  const [data, setData] = useState([
     {bookname: 'toto', bookauteur: 'Maxime K', bookpublication: '19/06/2020', bookavailable: true},
     {bookname: 'toto2', bookauteur: 'Budimir U',bookpublication: '19/06/2020', bookavailable: false}
-]
+  ]);
 
+  return (
+    <MaterialTable
+      title="Book List"
+      columns={columns}
+      data={data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              setData([...data, newData]);
+              
+              resolve();
+            }, 1000)
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataUpdate = [...data];
+              const index = oldData.tableData.id;
+              dataUpdate[index] = newData;
+              setData([...dataUpdate]);
 
-class RepositoryTable extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            items: []
-        };
-        this.onClick = props.onClick
-    }
-
-    componentDidMount() {
-
-        this.setState({
-            isLoaded: true,
-            items: items
-                });
-                /*
-        fetch("/api/repositories")
-            .then(res => res.json())
-            .then((result) => {
-
-                this.setState({
-                    isLoaded: true,
-                    items: result.data
-                });
-            },
-            (error) => {
-                this.setState({
-                isLoaded: true,
-                error
-            });
-            }
-        )*/
-    }
-
-
-
-  render() {
-    return (
-        <div>
-            <MaterialTable
-              title={"Book Market"}
-              data=fetch('https://api.github.com/users/hacktivist123/repos')
-  .then(response => response.json());
-              columns={columns}
-              options={options}
-              onRowClick={this.onClick}
-              localization={{
-                body: {
-                    emptyDataSourceMessage: 'Loading repositories...',
-                },
-                }}
-            />
-        </div>
-    );
-  }
-
-  }
+              resolve();
+            }, 1000)
+          }),
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataDelete = [...data];
+              const index = oldData.tableData.id;
+              dataDelete.splice(index, 1);
+              setData([...dataDelete]);
+              
+              resolve()
+            }, 1000)
+          }),
+      }}
+    />
+  )
+}
   export default RepositoryTable
